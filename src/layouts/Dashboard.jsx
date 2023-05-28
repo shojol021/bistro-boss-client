473934b4b665855911c40f0ec862db09a0178090
shojol021 <shojol021@gmail.com>
2023-05-28 15:12:46 +0600

    my cart page updated with delete function

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,22 +1,33 @@
-import { Link, Outlet } from "react-router-dom";
-import { FaShoppingCart } from "react-icons/fa";
+import { NavLink, Outlet } from "react-router-dom";
+import { FaCalendar, FaHome, FaShoppingCart } from "react-icons/fa";
+import { FaWallet } from "react-icons/fa";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+    const [cart] = useCart()
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
+            <div className="drawer-content flex flex-col items-center">
 
                 <Outlet></Outlet>
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
             </div>
-            <div className="drawer-side">
+            <div className="drawer-side ">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                <ul className="menu p-4 w-80 bg-[#D1A054] text-base-content ">
 
-                    <li><Link><FaShoppingCart>My Cart</FaShoppingCart></Link></li>
-                    <li><a>Sidebar Item 2</a></li>
+
+                    <li><NavLink to='/dashboard/home'><FaHome></FaHome>User Home</NavLink></li>
+                    <li><NavLink to='/dashboard/reservation'><FaCalendar></FaCalendar>Reservation</NavLink></li>
+                    <li><NavLink to='/dashboard/history'><FaWallet></FaWallet>Payment History</NavLink></li>
+                    <li><NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart 
+                    <div className="badge">{cart?.length || 0}</div></NavLink></li>
+                    <div className="divider"></div>
+                    <li><NavLink to='/'><FaHome></FaHome>Home</NavLink></li>
+                    <li><NavLink to='/menu'>Menu</NavLink></li>
+                    <li><NavLink to='/order'>Order</NavLink></li>
                 </ul>
 
             </div>
--- src/pages/dashboard/mycart/MyCart.jsx
+++ src/pages/dashboard/mycart/MyCart.jsx
@@ -1,9 +1,89 @@
+import { Helmet } from "react-helmet-async";
+import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const MyCart = () => {
+
+    const [cart, refetch] = useCart()
+    const total = cart.reduce((sum, item) => sum + item.price, 0)
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Your item will be removed from the cart",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, remove it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/cart/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire(
+                                'Item removed!',
+                                'Your item has been removed from the cart.',
+                                'success'
+                            )
+                        }
+                    })
+
+            }
+        })
+    }
     return (
-        <div>
-            my cart
+        <div className="p-12 w-full">
+            <Helmet>
+                <title>Bistro | My Cart</title>
+            </Helmet>
+            <div className="flex justify-between h-12">
+                <h3>Total Order: {cart.length}</h3>
+                <h3>Total Price: ${total}</h3>
+                <button className="btn btn-warning btn-sm">Pay</button>
+            </div>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Image</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, idx) => <tr key={
+                                item._id}>
+                                <th>{idx + 1}</th>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{item.name}</td>
+                                <td className="text-end">{item.price}</td>
+                                <th>
+                                    <button onClick={() => handleDelete(item._id)} className="btn btn-ghost btn-sm bg-red-700 text-white"><FaTrashAlt></FaTrashAlt></button>
+                                </th>
+                            </tr>)
+                        }
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
